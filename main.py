a636f6372083250177580051682e3513d0e73190
tbignardi <111576331+tbignardi@users.noreply.github.com>
2022-10-20 00:52:58 +0000

    Criar variáveis e exibi-las na tela.

--- main.py
+++ main.py
@@ -3,7 +3,16 @@
 # print() = comando de saída
 print("Hello World!")
 
+# Quando quiser guardar uma String!
+nome = "Thiago Bignardi"
+# Quando quiser guardar um número inteiro
+idade = 30
 
+# Exibir o meu nome (que está dentro da variável nome)
+print (nome)
+
+#Exibir a idade (que está dentro da variável idade)
+print (idade)
 
 '''
 
