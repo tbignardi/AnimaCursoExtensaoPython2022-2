1e77ebe0105265bbc4f7008a1d18b929c268e1a7
tbignardi <111576331+tbignardi@users.noreply.github.com>
2022-11-10 01:18:55 +0000

    Função imposto

--- aula3-2022-11-09b.py
+++ aula3-2022-11-09b.py
@@ -14,11 +14,36 @@ print(imposto2)
 #criar uma função calcular_imposto() que calcula um imposto de 5% e retorna a quem pediu
 #declaração da função (como ela funciona)
 def calcular_imposto(preco_produto):
-  imposto = preco_produto * 0.05
+  imposto = preco_produto * 0.07
   return imposto
 
 #aqui é o uso, imposto a calcular e exibir na tela
 preco = 299
 imposto = calcular_imposto(preco)
 print(imposto)
-print(preco_produto)
+
+#agora calcular imposto a alíquota 7%
+valores = [1.99, 24.50, 78.27, 1515.5]
+#se eu quiser calcular o imposto destes quatro valores e exibir na tela assim: "O imposto de... é..."
+
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto(valor)}")
+
+#declarar uma função calcular_imposto_aliquota que recebe dois parâmetros: o preço do produto e a alíquota de imposto a ser aplicada e retorna o imposto calculado. Se a alíquota não for informada, utilize 7% como padrão.
+
+def calcular_imposto_aliquota(valor, aliquota = 7):
+  imposto = valor * aliquota / 100
+  return imposto
+
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto_aliquota (valor)}")
+
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto_aliquota (valor, 7)}")
+  
+
+  #e se agora o imposto for 10%?
+
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto_aliquota (valor, 10)}")
+
